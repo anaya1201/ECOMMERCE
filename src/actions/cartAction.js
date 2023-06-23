001549e42f54554de21f04d0698bbd29f11c762e
anaya1201 <120556305+anaya1201@users.noreply.github.com>
2023-06-23 23:59:55 +0530

    Initial

--- src/actions/cartAction.js
+++ src/actions/cartAction.js
@@ -21,7 +21,7 @@ import {
       },
     });
   
-    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+    localStorage.setItem("cartIems", JSON.stringify(getState().cart.cartItems));
   };
   
   // REMOVE FROM CART
